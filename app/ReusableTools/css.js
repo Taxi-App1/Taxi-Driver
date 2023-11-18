b0975c5ed1f415232dca18b10d072a9c71bf90ae
Mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2023-11-18 07:49:56 +0200

    add the resuable tools for css and button and fieldSet input

---
+++ app/ReusableTools/css.js
@@ -0,0 +1,11 @@
+export const colors = {
+  primary: "#212429",
+  secondary: "#DCDFE4",
+  primaryYellow: "#ffb200",
+  secondaryYellow: "#ffd403",
+};
+
+export const fonts = {
+  regular: "Agrandi-Regular",
+  bold: "Agrandi-TextBold",
+};
