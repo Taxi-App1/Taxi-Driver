23792bd7f14a1bdc98328bbd699d563721b2eb6a
Mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2023-11-18 07:46:44 +0200

    add the translation for the app

--- app/Localization.js
+++ app/Localization.js
@@ -1,4 +1,161 @@
 export const translations = {
-  en: {},
-  ar: {},
+  en: {
+    cancel: "cancel",
+    signNav: {
+      signIn: "Sign In",
+      signUp: "Sing Up",
+    },
+    signUpDriver: {
+      input: {
+        first_name: {
+          label: "First Name",
+          placeholder: "Enter your first name",
+        },
+        last_name: {
+          label: "Last Name",
+          placeholder: "Enter your last name",
+        },
+        email: {
+          label: "Email",
+          placeholder: "Enter your email address",
+        },
+        phone: {
+          label: "Phone Number",
+          placeholder: "Enter your phone number",
+        },
+        password: {
+          label: "Password",
+          placeholder: "Enter your password",
+        },
+        confirm_password: {
+          label: "Confirm Password",
+          placeholder: "Confirm your password",
+        },
+      },
+      error: {
+        first_name: {
+          empty: "Please enter your first name",
+          length: "Firt name should be at least 3 characters",
+        },
+        last_name: {
+          empty: "Please enter your last name",
+          length: "Last name should be at least 3 characters",
+        },
+        email: {
+          empty: "Please enter your email address",
+          invalid: "Invalid email address",
+        },
+        phone: {
+          empty: "Please enter your phone number",
+          invalid: "Invalid phone number",
+          inUse:
+            "This phone number is already in use. Please choose a different one.",
+        },
+        password: {
+          emtpy: "Please enter your password",
+          invalid: "Password should contain at least 8 characters.",
+        },
+        confirm_password: {
+          empty: "Please confirm your password",
+          notMatch: "Passwords do not match",
+        },
+      },
+      addImage: {
+        text: "Add your profile image(optional):",
+        textButton: "Select Image",
+      },
+      button: {
+        signup: "Sign Up",
+        submitting: "Submitting...",
+      },
+    },
+    signInDriver: {
+      title: "Sign In",
+      text: "Log in to your existing account of Pickme up",
+      forgotPassword: "Forgot password?",
+      noAccount: "Don't have an account?",
+      signUp: "Sgin Up",
+      signIn: "Sign In",
+      forgotPass: "Forgot password?",
+    },
+  },
+  ar: {
+    cancel: "إلغاء",
+    signNav: {
+      signIn: "تسجيل دخول",
+      signUp: "تسجيل حاب",
+    },
+    signUpDriver: {
+      input: {
+        first_name: {
+          label: "الإسم الأول",
+          placeholder: "ادخل إسمك الأول",
+        },
+        last_name: {
+          label: "الإسم الأخير",
+          placeholder: "ادخل إسمك الأخير",
+        },
+        email: {
+          label: "ألبريد الإلكتروني",
+          placeholder: "ادخل بريدك الإلكتروني",
+        },
+        phone: {
+          label: "رقم الهاتف",
+          placeholder: "ادخل رقم هاتفك",
+          inUse: "رقم الهاتف هذا قيد الاستخدام بالفعل. الرجاء اختيار رقم آخر.",
+        },
+        password: {
+          label: "كلمة السر",
+          placeholder: "ادخل كلمة السر",
+        },
+        confirm_password: {
+          label: "تأكيد كلمة السر",
+          placeholder: "أكد كلمة السر",
+        },
+      },
+      error: {
+        first_name: {
+          empty: "الرجاء إدخال إسمك الأول",
+          length: "يجب أن يحتوي الإسم الأول على الأقل ثلاث أحرف",
+        },
+        last_name: {
+          empty: "الرجاء إدخال الإسم الأخير",
+          length: "يجب أن يحتوي الإسم الثاني على الأقل ثلاث أحرف",
+        },
+        email: {
+          empty: "الرجاء إدخال البريد الإلكتروني",
+          invalid: "بريد الكتروني غير صالح",
+        },
+        phone: {
+          empty: "الرجاء إدخال رقم الهاتف",
+          invalid: "رقم هاتف غير صالح",
+        },
+        password: {
+          emtpy: "الرجاء إدخال كلمة السر",
+          invalid: "يجب أن تحتوي كلمة المرور على 8 أحرف على الأقل.",
+        },
+        confirm_password: {
+          empty: "الرجاء تأكيد كلمة السر",
+          notMatch: "كلمة المرور غير مطابقة",
+        },
+      },
+      addImage: {
+        text: ":أضف صورتك الشخصية(إختياري)",
+        textButton: "إختر صورة",
+      },
+      button: {
+        signup: "تسجيل حساب",
+        submitting: "جاري التسجيل...",
+      },
+    },
+    signInDriver: {
+      title: "تسجيل الدخول",
+      text: "قم بتسجيل الدخول إلى حسابك الحالي في Pickme up",
+      forgotPassword: "نسيت كلمة السر؟",
+      noAccount: "ليس لديك حساب؟",
+      signUp: "اشترك",
+      signIn: "تسجيل دخول",
+      forgotPass: "نسيت كلمة السر؟",
+    },
+  },
 };
